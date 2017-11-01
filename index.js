6982408401699457d1b81139fdbe10e01f9685b5
james2doyle <james2doyle@gmail.com>
2017-10-31 20:21:14 -0700

    refactor to classes?

--- index.js
+++ index.js
@@ -4,91 +4,102 @@ const moment = require('moment');
 // load up the config for the app
 const config = require('./config.json');
 
-// get the times from the next week
-const start = moment().day(config.startWeek);
-const startFormatted = start.format('dddd, MMMM Do YYYY');
-
-const end = moment().day(config.endWeek);
-const endFormatted = end.format('dddd, MMMM Do YYYY');
-
-const JSONheaders = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json'
-};
-
-function holidays() {
-  if (!config.holidayUrl || config.holidayUrl.trim() === "") {
-    console.error('There was no holidayUrl set in the config. Exiting.');
-    process.exit(0);
+class App {
+  constructor(conf) {
+    this.config = conf;
   }
+  setupDates() {
+    this.start = moment().day(this.config.startWeek);
+    this.startFormatted = this.start.format('dddd, MMMM Do YYYY');
 
-  console.log(`checking for holidays between ${startFormatted} and ${endFormatted}`);
+    this.end = moment().day(this.config.endWeek);
+    this.endFormatted = this.end.format('dddd, MMMM Do YYYY');
+    return this;
+  }
+  makeHolidayRequest() {
+    if (!this.config.holidayUrl || this.config.holidayUrl.trim() === "") {
+      console.error('There was no holidayUrl set in the config. Exiting.');
+      process.exit(0);
+    }
 
-  return new Promise(function(resolve, reject) {
-    request(config.holidayUrl, {
-        method: 'get',
-        headers: JSONheaders
-      })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        const yearHolidays = res[moment().year()];
-        if (typeof(yearHolidays) === 'undefined') {
-          resolve();
-        }
+    console.log(`checking for holidays between ${this.startFormatted} and ${this.endFormatted}`);
 
-        const validHolidays = yearHolidays.filter((day) => {
-          // does the day belong in the nexy week?
-          return moment(day).isBetween(start, end);
+    return new Promise((resolve, reject) => {
+      request(this.config.holidayUrl, {
+          method: 'get',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          const yearHolidays = res[moment().year()];
+          if (typeof(yearHolidays) === 'undefined') {
+            resolve([]);
+          }
+
+          const validHolidays = yearHolidays.filter((day) => {
+            // does the day belong in the nexy week?
+            return moment(day).isBetween(this.start, this.end);
+          });
+
+          const attachments = validHolidays.map((day) => {
+            const dayFormatted = moment(day).format('dddd, MMMM Do YYYY');
+
+            return {
+              fallback: `${dayFormatted} is a holiday next week.`,
+              color: "#c93742",
+              title: `${dayFormatted} is a holiday next week.`
+            };
+          });
+
+          resolve(attachments);
         });
+    });
+  }
+  postToSlack(attachments) {
+    if (attachments.length < 1) {
+      console.log('There were no holidays found for the upcoming week.');
+      return;
+    }
 
-        const attachments = validHolidays.map((day) => {
-          const dayFormatted = moment(day).format('dddd, MMMM Do YYYY');
+    const prefix = (process.env.NODE_ENV != 'production') ? "======TESTING======\n" : '<!channel> ';
 
-          return {
-            fallback: `${dayFormatted} is a holiday next week.`,
-            color: "#c93742",
-            title: `${dayFormatted} is a holiday next week.`
-          };
-        });
+    const message = {
+      username: this.config.botName,
+      icon_emoji: this.config.slackEmoji,
+      text: `${prefix}Here are your holiday reminders for the upcoming week! :tada:`,
+      attachments
+    };
 
-        resolve(attachments);
-      });
-  });
+    if (process.env.NODE_ENV != 'production') {
+      console.log(message);
+      return;
+    }
+
+    return request(this.config.slackUrl, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    });
+  }
 }
 
-function postToSlack(attachments) {
-  if (attachments.length < 1) {
-    console.log('There were no holidays found for the upcoming week.');
-    return;
-  }
+const app = new App(config);
 
-  const prefix = (process.env.NODE_ENV != 'production') ? "======TESTING======\n" : '<!channel> ';
-
-  const message = {
-    username: config.botName,
-    icon_emoji: config.slackEmoji,
-    text: `${prefix}Here are your holiday reminders for the upcoming week! :tada:`,
-    attachments
-  };
-
-  if (process.env.NODE_ENV != 'production') {
-    console.log(message);
-    return;
-  }
-
-  return request(config.slackUrl, {
-    method: 'post',
-    headers: JSONheaders,
-    body: JSON.stringify(message)
-  });
-}
-
-holidays()
-  .then(postToSlack)
+app
+  .setupDates()
+  .makeHolidayRequest()
+  .then(app.postToSlack)
   .catch((err) => {
-    console.log(err);
+    console.error(err);
+    process.exit(0);
   });
 
 process.on('uncaughtException', (err) => {
