05cfbc001eedd24d2354c59cb317198ea708c339
james2doyle <james2doyle@gmail.com>
2017-11-07 15:56:26 -0800

    change color of holiday attachment. format js

--- src/lib/fetchHolidays.js
+++ src/lib/fetchHolidays.js
@@ -4,40 +4,37 @@ import request from 'node-fetch';
 // load up the config for the app
 import config from '../config';
 
-export default function() {
-  if (!config.holidayUrl || config.holidayUrl.trim() === "") {
+export default function () {
+  if (!config.holidayUrl || config.holidayUrl.trim() === '') {
     console.error('There was no holidayUrl set in the config. Exiting.');
     process.exit(0);
   }
 
   console.log(`checking for holidays between ${config.startFormatted} and ${config.endFormatted}`);
 
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     request(config.holidayUrl, {
-        method: 'get',
-        headers: config.JSONheaders
-      })
-      .then((res) => {
-        return res.json();
-      })
+      method: 'get',
+      headers: config.JSONheaders,
+    })
+      .then(res => res.json())
       .then((res) => {
         const yearHolidays = res[moment().year()];
-        if (typeof(yearHolidays) === 'undefined') {
+        if (typeof (yearHolidays) === 'undefined') {
           resolve();
         }
 
-        const validHolidays = yearHolidays.filter((day) => {
+        const validHolidays = yearHolidays.filter(day =>
           // does the day belong in the nexy week?
-          return moment(day).isBetween(config.start, config.end);
-        });
+           moment(day).isBetween(config.start, config.end));
 
         const attachments = validHolidays.map((day) => {
           const dayFormatted = moment(day).format('dddd, MMMM Do YYYY');
 
           return {
             fallback: `${dayFormatted} is a holiday next week.`,
-            color: "#c93742",
-            title: `${dayFormatted} is a holiday next week.`
+            color: '#acc23a',
+            title: `${dayFormatted} is a holiday next week.`,
           };
         });
 
